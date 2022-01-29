7a9ead6582f251095c79a05eb1b339cc9d0882f7
johnylogic <johns.baby@mca.christuniversity.in>
2022-01-29 09:58:04 +0530

    color

--- main.js
+++ main.js
@@ -11,21 +11,28 @@ var specialKeys = new Array();
     function test_str() 
     {
         var res;
-        var str =
-            document.getElementById("password").value;
-        if (str.match(/[a-z]/g) && str.match(
-                /[A-Z]/g) && str.match(
-                /[0-9]/g) && str.match(
-                /[^a-zA-Z\d]/g) && str.length >= 8)
-            console.log("Valid password")
+        var str = document.getElementById("password").value;
+        var eval =0, pval=0;
+        if (str.match(/[a-z]/g) && str.match(/[A-Z]/g) && str.match(/[0-9]/g) && str.match(/[^a-zA-Z\d]/g) && str.length >= 8)
+        {
+            console.log("Valid password");
+            pval=1
+        }
         else
             alert("Invalid password")
         usr = document.getElementById("email-address").value;
         if(usr.match(/^([a-z0-9_\.]+)@(([a-z0-9]+)\.){2}([a-z]{2,3})$/))
-                console.log("Valid email")
+        {
+                console.log("Valid email");
+                eval=1;
+        }
         else
                 alert("Invalid email")
-
+        
+        if(pval==1 && eval==1)
+        {
+            window.location.href("feedbackpage.xhtml");
+        }
     }
 
     function reset() 
